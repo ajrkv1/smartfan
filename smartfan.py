968dfb907731f83a317412ac0894a44c76c793b8
ajrkv1 <shaharma03@gmail.com>
2022-07-22 17:51:11 +0300

    added 'fan' resource

--- smartfan.py
+++ smartfan.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 api = Api(app)
 
 light = False
+fan = 0
 
 class Light(Resource):
     def get(self):
@@ -14,8 +15,18 @@ class Light(Resource):
         light = not light
         return self.get()
 
+class Fan(Resource):
+    
+    def get(self):
+        return {'fan': fan}
+    
+    def post(self,fan_power):
+        global fan
+        fan = fan_power
+        return {'fan': fan}
 
 api.add_resource(Light,'/light')
+api.add_resource(Fan,'/fan','/fan/<int:fan_power>')
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
