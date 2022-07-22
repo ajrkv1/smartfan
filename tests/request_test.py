0a88a0988cafd04172eacb92667d916ebbcf4f64
ajrkv1 <shaharma03@gmail.com>
2022-07-22 18:47:29 +0300

    added tests for fan

--- tests/request_test.py
+++ tests/request_test.py
@@ -1,12 +1,14 @@
 import unittest
+from unittest import TestCase
 import requests
+
 
 HOST = 'privatepi'
 PORT = 5000
 
-class LightTestCase(unittest.TestCase):
+class LightTestCase(TestCase):
 
-    def test_initial(self):
+    def test_get(self):
         res = requests.get(f'http://{HOST}:{PORT}/light')
         self.assertFalse(res.json()['light'])
     
@@ -15,6 +17,19 @@ class LightTestCase(unittest.TestCase):
         self.assertTrue(res.json()['light'])
         res = requests.post(f'http://{HOST}:{PORT}/light')
         self.assertFalse(res.json()['light'])
+
+class FanTestCase(TestCase):
+    def test_get(self):
+        res = requests.get(f'http://{HOST}:{PORT}/fan')
+        self.assertEqual(res.json()['fan'],0)
+    
+    def test_post(self):
+        res = requests.post(f'http://{HOST}:{PORT}/fan',json={"power":2},headers={"Content-Type": "application/json"})
+        self.assertEqual(res.json()['fan'],2)
+        res = requests.post(f'http://{HOST}:{PORT}/fan',json={"power":0},headers={"Content-Type": "application/json"})
+        self.assertEqual(res.json()['fan'],0)
+        res = requests.post(f'http://{HOST}:{PORT}/fan',json={"power":5},headers={"Content-Type": "application/json"})
+        self.assertEqual(res.json()['status'],400)
 
 
 if __name__ == '__main__':
